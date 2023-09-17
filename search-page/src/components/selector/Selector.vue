1c12ccbab11b9d9000c292ddf1340511859f26e9
slim123fk <>
2023-09-17 16:08:29 +1000

    finish the basic dropdown menu components

---
+++ search-page/src/components/selector/Selector.vue
@@ -0,0 +1,50 @@
+<template>
+    <el-select v-model="value" filterable :placeholder="select.name" @change="change">
+    <el-option
+      v-for="item in this.select.data"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+    export default {
+        name: "Selector",
+        data() {
+      return {
+        value: ''
+      }
+    },
+    computed:{
+        get(){
+            this.select.result && this.select.change(this.select.result)
+            return this.select.result
+        },
+        set(){
+
+        }
+    },
+    props: {
+        select:Object
+    },
+    methods:{
+        change(val){
+            console.log(val)
+            this.select.result = val
+            this.select.change && this.select.change(this.buildChildern(val))
+        },
+        buildChildern(val){
+            let data = this.select.data.find(item=>{
+                return item.value==val
+            })
+
+            return data.children
+        }
+    }
+    }
+</script>
+
+<style scoped>
+</style>
--- search-page/src/main.js
+++ search-page/src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(axios, VueAxios)
 
 new Vue({
   router,
